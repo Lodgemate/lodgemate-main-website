284785392ce23c812d9ec3da5bc50ad2f177704b
StephenChi-hi <nwikechisom77@gmail.com>
2024-08-28 15:25:51 +0100

    UI update

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -5,10 +5,10 @@ import MobileChat from "./MobileChat";
 function Chat() {
   return (
     <div className=" flex justify-center  text-[14px]">
-      <div className="lg:block- hidden w-full max-w-[1200px] border">
+      <div className="lg:block hidden w-full max-w-[1200px] border">
         <DesktopChat />
       </div>
-      <div className="lg:hidden- w-full max-w-[1200px] border block">
+      <div className="lg:hidden w-full max-w-[1200px] border block">
         {" "}
         <MobileChat />
       </div>

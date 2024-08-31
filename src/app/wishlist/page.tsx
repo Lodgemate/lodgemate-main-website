78ee91090f230d01951bb9285d62798170c4beda
StephenChi-hi <nwikechisom77@gmail.com>
2024-08-31 13:21:06 +0100

    UI -FIX

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -43,23 +43,28 @@ function Wishlist() {
 
   return (
     <div className="p-4 mt-[100px] min-h-screen sm:px-[100px] text-[14px] text-lblack ">
+      <h1 className="text-[16px] pb-4 sm:hidden block">
+        {tabData[activeTab].message}
+      </h1>
+
       <div className="flex justify-between items-center">
-        <h1 className="text-[16px]">{tabData[activeTab].message}</h1>
-        <div className="flex  bg-[#F8F8F8] p-1 rounded-[8px]">
-          {Object.keys(tabData).map((tab) => (
-            <button
-              key={tab}
-              className={`px-4 py-2 rounded ${
-                activeTab === tab
-                  ? "bg-primary text-white"
-                  : " text-lblack"
+        <h1 className="text-[16px] sm:block hidden">
+          {tabData[activeTab].message}
+        </h1>
+        <div className="flex  bg-[#F8F8F8] p-1 rounded overflow-x-auto  no-scrollbar">
+          <div className="flex items-center min-w-[400px]">
+            {Object.keys(tabData).map((tab) => (
+              <button
+                key={tab}
+                className={`px-4 py-2 rounded ${
+                  activeTab === tab ? "bg-primary text-white" : " text-lblack"
                 }`}
-              
-              onClick={() => setActiveTab(tab)}
-            >
-              {tab}
-            </button>
-          ))}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
       <div className="mt-4">{tabData[activeTab].content}</div>

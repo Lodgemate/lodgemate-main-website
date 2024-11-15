45085555f65914f11ad9c2f1c5d0579098da9cde
ChikenduHillary <chikenduhillarychiagoziemu@gmail.com>
2024-11-15 20:48:09 +0100

    add redirect on login

--- src/app/auth/login/Form.tsx
+++ src/app/auth/login/Form.tsx
@@ -68,7 +68,7 @@ const LoginForm: React.FC = () => {
         dispatch(setToken(response.data.token));
         localStorage.setItem("token", response.data.token);
 
-        // router.push("/");
+        router.push("/");
       } else {
         toast({
           variant: "destructive",
